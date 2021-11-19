d03a1c66466f6f9e77648bdb116191f82a40337e
FacundoaQuiroga <facundoaquiroga1@gmail.com>
2021-11-19 19:22:12 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/about', function () {
+    return 'hola soy about';
+});
+
+Route::view('profile', 'profile');
+
+Route::post('profile', [App\Http\Controllers\ProfileController::class, 'upload']);
+
+
+
